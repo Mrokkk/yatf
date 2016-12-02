920f15095ff86bf7554370b9c37338841cab4faf
Mrokkk <mrokk@protonmail.com>
2016-12-03 00:19:05 +0100

    Improvements

--- include/yatf.h
+++ include/yatf.h
@@ -8,75 +8,75 @@ using tests_printer = int (*)(const char *, ...);
 
 namespace detail {
 
-// Minimal version of inherited_list
-template <typename Type>
-class tests_list {
-
-    Type *_prev, *_next;
-
-    void add_element(Type &new_element, Type &prev, Type &next) {
-        next._prev = &new_element;
-        prev._next = &new_element;
-        new_element._next = &next;
-        new_element._prev = &prev;
-    }
-
-    operator Type &() {
-        return *reinterpret_cast<Type *>(this);
-    }
-
-public:
-
-    class iterator {
-
-        Type *_ptr = nullptr;
-
-    public:
-
-        iterator(Type *t)
-            : _ptr(t) {}
-
-        iterator &operator++() {
-            _ptr = _ptr->next();
-            return *this;
-        }
-
-        Type &operator*() {
-            return *_ptr;
-        }
-
-        bool operator!=(const iterator &comp) {
-            return _ptr != comp._ptr;
-        }
-
-    };
-
-    tests_list() {
-        _next = _prev = reinterpret_cast<Type *>(this);
-    }
-
-    Type &add(Type *new_element) {
-        add_element(*new_element, *_prev, *this);
-        return *this;
-    }
-
-    Type *next() {
-        return _next == this ? nullptr : _next;
-    }
-
-    auto begin() {
-        return iterator(_next);
-    }
-
-    auto end() {
-        return iterator(reinterpret_cast<Type *>(this));
-    }
-
-};
-
 extern tests_printer _print;
 
 struct test_session final {
+
+    // Minimal version of inherited_list
+    template <typename Type>
+    class tests_list {
+
+        Type *_prev, *_next;
+
+        void add_element(Type &new_element, Type &prev, Type &next) {
+            next._prev = &new_element;
+            prev._next = &new_element;
+            new_element._next = &next;
+            new_element._prev = &prev;
+        }
+
+        operator Type &() {
+            return *reinterpret_cast<Type *>(this);
+        }
+
+    public:
+
+        class iterator {
+
+            Type *_ptr = nullptr;
+
+        public:
+
+            iterator(Type *t)
+                : _ptr(t) {}
+
+            iterator &operator++() {
+                _ptr = _ptr->next();
+                return *this;
+            }
+
+            Type &operator*() {
+                return *_ptr;
+            }
+
+            bool operator!=(const iterator &comp) {
+                return _ptr != comp._ptr;
+            }
+
+        };
+
+        tests_list() {
+            _next = _prev = reinterpret_cast<Type *>(this);
+        }
+
+        Type &add(Type *new_element) {
+            add_element(*new_element, *_prev, *this);
+            return *this;
+        }
+
+        Type *next() {
+            return _next == this ? nullptr : _next;
+        }
+
+        auto begin() {
+            return iterator(_next);
+        }
+
+        auto end() {
+            return iterator(reinterpret_cast<Type *>(this));
+        }
+
+    };
 
     class test_case final : public tests_list<test_case> {
 
@@ -88,15 +88,15 @@ struct test_session final {
         const char *_fail_message = "\e[31m[  FAIL  ]\e[0m";
 
         void print_test_start_message() {
-            yatf::detail::_print("%s %s.%s\n", _run_message, _suite_name, _test_name);
+            _print("%s %s.%s\n", _run_message, _suite_name, _test_name);
         }
 
         void print_test_result() {
             if (failed)
-                yatf::detail::_print("%s ", _fail_message);
+                _print("%s ", _fail_message);
             else
-                yatf::detail::_print("%s ", _pass_message);
-            yatf::detail::_print("%s.%s (%u assertions)\n\n", _suite_name, _test_name, assertions);
+                _print("%s ", _pass_message);
+            _print("%s.%s (%u assertions)\n\n", _suite_name, _test_name, assertions);
         }
 
     public:
@@ -109,9 +109,10 @@ struct test_session final {
             test_session::get().register_test(this);
         }
 
-        void assert(bool cond) {
+        bool assert(bool cond) {
             ++assertions;
             if (!cond) ++failed;
+            return cond;
         }
 
         int call() {
@@ -144,14 +145,14 @@ public:
     int run() {
         unsigned failed = 0;
         unsigned test_cases = 0;;
-        yatf::detail::_print("\e[32m[========]\e[0m Running %u test cases\n\n", _tests_number);
+        _print("\e[32m[========]\e[0m Running %u test cases\n\n", _tests_number);
         for (auto &test : _test_cases) {
             _current_test_case = &test;
             if (test.call()) failed++;
             test_cases++;
         }
-        yatf::detail::_print("\e[32m[========]\e[0m Passed %u test cases\n", test_cases - failed);
-        if (failed) yatf::detail::_print("\e[31m[========]\e[0m Failed %u test cases\n", failed);
+        _print("\e[32m[========]\e[0m Passed %u test cases\n", test_cases - failed);
+        if (failed) _print("\e[31m[========]\e[0m Failed %u test cases\n", failed);
         return failed;
     }
 
@@ -165,15 +166,13 @@ public:
 
 #define REQUIRE(cond) \
     { \
-        yatf::detail::test_session::get().current_test_case().assert(cond); \
-        if (!(cond)) \
+        if (!yatf::detail::test_session::get().current_test_case().assert(cond)) \
             yatf::detail::_print("assertion failed: %s:%d: \'%s\' is false\n", __FILE__, __LINE__, #cond); \
     }
 
 #define REQUIRE_FALSE(cond) \
     { \
-        yatf::detail::test_session::get().current_test_case().assert(!(cond)); \
-        if (cond) \
+        if (!yatf::detail::test_session::get().current_test_case().assert(!(cond))) \
             yatf::detail::_print("assertion failed: %s:%d: \'%s\' is true\n", __FILE__, __LINE__, #cond); \
     }
 
@@ -194,7 +193,7 @@ tests_printer _print;
 
 } // namespace detail
 
-int main(tests_printer printer) {
+inline int main(tests_printer printer) {
     detail::_print = printer;
     return detail::test_session::get().run();
 }
--- tests/self_tests.cpp
+++ tests/self_tests.cpp
@@ -1,7 +1,7 @@
 #include "../include/yatf.h"
 
 TEST(self_tests, require_works) {
-    REQUIRE(1 == 1);
-    REQUIRE_FALSE(0 == 1);
+    REQUIRE(true);
+    REQUIRE_FALSE(false);
 }
 
