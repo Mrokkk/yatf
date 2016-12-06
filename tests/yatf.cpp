80869247ad573e18fb469f06d6b43d9245fced51
Mrokkk <mrokk@protonmail.com>
2016-12-06 22:01:47 +0100

    Some improvements in tests

--- tests/yatf.cpp
+++ tests/yatf.cpp
@@ -3,6 +3,7 @@
 #define BOOST_TEST_MODULE yatf_tests
 #include <boost/test/unit_test.hpp>
 #include <cstdarg>
+#include <string>
 
 char buffer[1024];
 int position = 0;
@@ -17,6 +18,11 @@ int print(const char *fmt, ...) {
     return i;
 }
 
+std::string get_buffer() {
+    position = 0;
+    return std::string(buffer);
+}
+
 namespace yatf {
 namespace detail {
 
@@ -29,49 +35,60 @@ printf_t _printf = print;
 BOOST_AUTO_TEST_CASE(test_can_pass) {
     yatf::detail::test_session::test_case tc{"suite", "name", nullptr};
     yatf::detail::test_session::get().current_test_case(&tc);
-    BOOST_CHECK(tc.assertions == 0);
-    BOOST_CHECK(tc.failed == 0);
+    BOOST_CHECK_EQUAL(tc.assertions, 0);
+    BOOST_CHECK_EQUAL(tc.failed, 0);
+    BOOST_CHECK_EQUAL(get_buffer(), "");
     REQUIRE_EQ(0, 0);
-    BOOST_CHECK(tc.assertions == 1);
-    BOOST_CHECK(tc.failed == 0);
+    BOOST_CHECK_EQUAL(tc.assertions, 1);
+    BOOST_CHECK_EQUAL(tc.failed, 0);
+    BOOST_CHECK_EQUAL(get_buffer(), "");
     REQUIRE(true);
-    BOOST_CHECK(tc.assertions == 2);
-    BOOST_CHECK(tc.failed == 0);
+    BOOST_CHECK_EQUAL(tc.assertions, 2);
+    BOOST_CHECK_EQUAL(tc.failed, 0);
+    BOOST_CHECK_EQUAL(get_buffer(), "");
     REQUIRE_FALSE(false);
-    BOOST_CHECK(tc.assertions == 3);
-    BOOST_CHECK(tc.failed == 0);
+    BOOST_CHECK_EQUAL(tc.assertions, 3);
+    BOOST_CHECK_EQUAL(tc.failed, 0);
+    BOOST_CHECK_EQUAL(get_buffer(), "");
 }
 
 BOOST_AUTO_TEST_CASE(test_can_fail) {
     yatf::detail::test_session::test_case tc{"suite", "name", nullptr};
     yatf::detail::test_session::get().current_test_case(&tc);
-    BOOST_CHECK(tc.assertions == 0);
-    BOOST_CHECK(tc.failed == 0);
+    BOOST_CHECK_EQUAL(tc.assertions, 0);
+    BOOST_CHECK_EQUAL(tc.failed, 0);
+    BOOST_CHECK_EQUAL(get_buffer(), "");
     REQUIRE_EQ(0, 1);
-    BOOST_CHECK(tc.assertions == 1);
-    BOOST_CHECK(tc.failed == 1);
+    BOOST_CHECK_EQUAL(tc.assertions, 1);
+    BOOST_CHECK_EQUAL(tc.failed, 1);
+    std::string comp("assertion failed: " + std::string(__FILE__) + ":" + std::to_string(__LINE__ - 3) + " \'0\' isn't \'1\': 0 != 1\n");
+    BOOST_CHECK_EQUAL(get_buffer(), comp);
     REQUIRE(false);
-    BOOST_CHECK(tc.assertions == 2);
-    BOOST_CHECK(tc.failed == 2);
+    BOOST_CHECK_EQUAL(tc.assertions, 2);
+    BOOST_CHECK_EQUAL(tc.failed, 2);
+    comp = std::string("assertion failed: " + std::string(__FILE__) + ":" + std::to_string(__LINE__ - 3) + " \'false\' is false\n");
+    BOOST_CHECK_EQUAL(get_buffer(), comp);
     REQUIRE_FALSE(true);
-    BOOST_CHECK(tc.assertions == 3);
-    BOOST_CHECK(tc.failed == 3);
+    BOOST_CHECK_EQUAL(tc.assertions, 3);
+    BOOST_CHECK_EQUAL(tc.failed, 3);
+    comp = std::string("assertion failed: " + std::string(__FILE__) + ":" + std::to_string(__LINE__ - 3) + " \'true\' is true\n");
+    BOOST_CHECK_EQUAL(get_buffer(), comp);
 }
 
 BOOST_AUTO_TEST_CASE(test_can_fail_and_pass) {
     yatf::detail::test_session::test_case tc{"suite", "name", nullptr};
     yatf::detail::test_session::get().current_test_case(&tc);
-    BOOST_CHECK(tc.assertions == 0);
-    BOOST_CHECK(tc.failed == 0);
+    BOOST_CHECK_EQUAL(tc.assertions, 0);
+    BOOST_CHECK_EQUAL(tc.failed, 0);
     REQUIRE_EQ(0, 0);
-    BOOST_CHECK(tc.assertions == 1);
-    BOOST_CHECK(tc.failed == 0);
+    BOOST_CHECK_EQUAL(tc.assertions, 1);
+    BOOST_CHECK_EQUAL(tc.failed, 0);
     REQUIRE(false);
-    BOOST_CHECK(tc.assertions == 2);
-    BOOST_CHECK(tc.failed == 1);
+    BOOST_CHECK_EQUAL(tc.assertions, 2);
+    BOOST_CHECK_EQUAL(tc.failed, 1);
     REQUIRE_FALSE(false);
-    BOOST_CHECK(tc.assertions == 3);
-    BOOST_CHECK(tc.failed == 1);
+    BOOST_CHECK_EQUAL(tc.assertions, 3);
+    BOOST_CHECK_EQUAL(tc.failed, 1);
 }
 
 void sample_test_case() {
@@ -84,8 +101,8 @@ BOOST_AUTO_TEST_CASE(test_can_call) {
     yatf::detail::test_session::test_case tc{"suite", "name", sample_test_case};
     yatf::detail::test_session::get().current_test_case(&tc);
     auto result = tc.call();
-    BOOST_CHECK(result == 2);
-    BOOST_CHECK(tc.assertions == 3);
-    BOOST_CHECK(tc.failed == 2);
+    BOOST_CHECK_EQUAL(result, 2);
+    BOOST_CHECK_EQUAL(tc.assertions, 3);
+    BOOST_CHECK_EQUAL(tc.failed, 2);
 }
 
