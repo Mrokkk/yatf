5e4ff8399333bf40053c1f087d8407cc2e99c60d
Mrokkk <mrokk@protonmail.com>
2017-04-30 12:55:16 +0200

    Add test for running one test

--- tests/test_session.cpp
+++ tests/test_session.cpp
@@ -73,5 +73,30 @@ BOOST_FIXTURE_TEST_CASE(can_run_tests, yatf_fixture) {
     _printf = print;
 }
 
+BOOST_FIXTURE_TEST_CASE(can_run_one_test, yatf_fixture) {
+    srand(time(NULL));
+    std::vector<std::unique_ptr<test_session::test_case>> tests;
+    std::vector<yatf::config> configs{
+            yatf::config{false, false, false},
+            yatf::config{true, false, false},
+            yatf::config{true, true, false},
+            yatf::config{false, true, false},
+            yatf::config{false, true, true},
+            yatf::config{false, false, true},
+            yatf::config{true, false, true},
+            yatf::config{true, true, true}
+    };
+    _printf = stubbed_printf;
+    for (auto c = configs.begin(); c != ++configs.begin(); ++c) {
+        tests.push_back(std::make_unique<test_session::test_case>("t", "t1", passing_test_case));
+        BOOST_CHECK_EQUAL(0, test_session::get().run(*c, "t.t1"));
+        tests.push_back(std::make_unique<test_session::test_case>("t", "t2", failing_test_case));
+        BOOST_CHECK_EQUAL(3, test_session::get().run(*c, "t.t2"));
+        BOOST_CHECK(test_session::get().run(*c, "t.t3") < 0);
+        BOOST_CHECK(test_session::get().run(*c, "some_bad_name") < 0);
+    }
+    _printf = print;
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
