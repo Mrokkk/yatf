60f14800b859c96a36f72ccdfb3435333ae668c9
Mrokkk <mrokk@protonmail.com>
2017-06-09 17:19:13 +0200

    Add yatf_mock.hpp

---
+++ include/yatf_mock.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <type_traits>
+
+namespace yatf {
+
+namespace detail {
+
+template <typename T>
+class mock_return_value final {
+
+    unsigned char data_[sizeof(T)];
+    T *value_ = reinterpret_cast<T *>(data_);
+
+public:
+
+    void set(const T &val) {
+        value_ = new(data_) T(val);
+    }
+
+    T &get() const {
+        return *value_;
+    }
+
+};
+
+template <>
+class mock_return_value<void> {};
+
+template <typename T>
+class mock {};
+
+template <typename R, typename ...Args>
+class mock<R(Args...)> final {
+
+    std::size_t nr_of_calls_ = 0;
+    mock_return_value<R> default_return_value_;
+
+public:
+
+    template <typename T = R>
+    typename std::enable_if<
+        std::is_void<T>::value, T
+    >::type operator()(Args ...) {
+        ++nr_of_calls_;
+    }
+
+    template <typename T = R>
+    typename std::enable_if<
+        !std::is_void<T>::value, T
+    >::type operator()(Args ...) {
+        ++nr_of_calls_;
+        return default_return_value_.get();
+    }
+
+    template <typename T = R>
+    typename std::enable_if<
+        !std::is_void<T>::value, void
+    >::type set_default_return_value(const T &val) {
+        default_return_value_.set(val);
+    }
+
+    std::size_t nr_of_calls() const {
+        return nr_of_calls_;
+    }
+
+};
+
+} // namespace yatf
+
+} // namespace detail
+
